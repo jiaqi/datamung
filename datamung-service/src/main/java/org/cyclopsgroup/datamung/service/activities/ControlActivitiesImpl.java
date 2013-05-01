c6d1ac2f60a786116196ca3911c003ad9ccf6477
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2013-05-01 01:22:25 +0000

    Workflow under development

---
+++ datamung-service/src/main/java/org/cyclopsgroup/datamung/service/activities/ControlActivitiesImpl.java
@@ -0,0 +1,35 @@
+package org.cyclopsgroup.datamung.service.activities;
+
+import org.cyclopsgroup.datamung.swf.interfaces.ControlActivities;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.simpleworkflow.flow.ActivityExecutionContextProvider;
+import com.amazonaws.services.simpleworkflow.flow.ActivityExecutionContextProviderImpl;
+
+@Component( "workflow.ControlActivities" )
+public class ControlActivitiesImpl
+    implements ControlActivities
+{
+    private final ActivityExecutionContextProvider contextProvider =
+        new ActivityExecutionContextProviderImpl();
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String createSnapshotName( String instanceName )
+    {
+        return instanceName + "-" + new DateTime().toString( "yyMMdd-HHmm" );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String createTempInstanceName( String snapshotName )
+    {
+        return "dm-"
+            + contextProvider.getActivityExecutionContext().getWorkflowExecution().getWorkflowId();
+    }
+}
---
+++ datamung-service/src/main/java/org/cyclopsgroup/datamung/service/activities/RdsActivitiesImpl.java
@@ -0,0 +1,147 @@
+package org.cyclopsgroup.datamung.service.activities;
+
+import java.io.IOException;
+
+import org.cyclopsgroup.datamung.api.types.DataArchive;
+import org.cyclopsgroup.datamung.api.types.Identity;
+import org.cyclopsgroup.datamung.swf.interfaces.RdsActivities;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.AmazonWebServiceRequest;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.rds.AmazonRDS;
+import com.amazonaws.services.rds.model.CreateDBSnapshotRequest;
+import com.amazonaws.services.rds.model.DeleteDBInstanceRequest;
+import com.amazonaws.services.rds.model.DeleteDBSnapshotRequest;
+import com.amazonaws.services.rds.model.DescribeDBInstancesRequest;
+import com.amazonaws.services.rds.model.DescribeDBInstancesResult;
+import com.amazonaws.services.rds.model.DescribeDBSnapshotsRequest;
+import com.amazonaws.services.rds.model.DescribeDBSnapshotsResult;
+import com.amazonaws.services.rds.model.RestoreDBInstanceFromDBSnapshotRequest;
+
+@Component( "workflow.RdsActivities" )
+public class RdsActivitiesImpl
+    implements RdsActivities
+{
+    private static <T extends AmazonWebServiceRequest> T decorate( T request,
+                                                                   Identity id )
+    {
+        AWSCredentials creds;
+        if ( id.getAwsAccessToken() == null )
+        {
+            creds =
+                new BasicAWSCredentials( id.getAwsAccessKeyId(),
+                                         id.getAwsSecretKey() );
+        }
+        else
+        {
+            creds =
+                new BasicSessionCredentials( id.getAwsAccessKeyId(),
+                                             id.getAwsSecretKey(),
+                                             id.getAwsAccessToken() );
+        }
+        request.setRequestCredentials( creds );
+        return request;
+    }
+
+    @Autowired
+    private AmazonRDS rds;
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void createSnapshot( String snapshotName, String instanceName,
+                                Identity identity )
+    {
+
+        rds.createDBSnapshot( decorate( new CreateDBSnapshotRequest(
+                                                                     snapshotName,
+                                                                     instanceName ),
+                                        identity ) );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void deleteSnapshot( String snapshotName, Identity identity )
+    {
+        rds.deleteDBSnapshot( decorate( new DeleteDBSnapshotRequest(
+                                                                     snapshotName ),
+                                        identity ) );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void dumpAndArchive( String instanceName, DataArchive archive,
+                                Identity identity )
+        throws IOException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String getInstanceStatus( String instanceName, Identity identity )
+    {
+        DescribeDBInstancesResult result =
+            rds.describeDBInstances( decorate( new DescribeDBInstancesRequest().withDBInstanceIdentifier( instanceName ),
+                                               identity ) );
+        if ( result.getDBInstances().isEmpty() )
+        {
+            return null;
+        }
+        return result.getDBInstances().get( 0 ).getDBInstanceStatus();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String getSnapshotStatus( String snapshotName, Identity identity )
+    {
+        DescribeDBSnapshotsResult result =
+            rds.describeDBSnapshots( decorate( new DescribeDBSnapshotsRequest().withDBSnapshotIdentifier( snapshotName ),
+                                               identity ) );
+        if ( result.getDBSnapshots().isEmpty() )
+        {
+            return null;
+        }
+        return result.getDBSnapshots().get( 0 ).getStatus();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void restoreSnapshot( String snapshotName, String instanceName,
+                                 Identity identity )
+    {
+        // TODO Set security group properly
+        rds.restoreDBInstanceFromDBSnapshot( decorate( new RestoreDBInstanceFromDBSnapshotRequest(
+                                                                                                   instanceName,
+                                                                                                   snapshotName ).withPubliclyAccessible( true ),
+                                                       identity ) );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void terminateInstance( String instanceName, Identity identity )
+    {
+        rds.deleteDBInstance( decorate( new DeleteDBInstanceRequest(
+                                                                     instanceName ),
+                                        identity ) );
+    }
+
+}
