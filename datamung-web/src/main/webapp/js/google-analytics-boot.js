69d4d0d701db090534f0cdb70d3e51fa9f7c054e
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2013-08-19 21:21:45 -0500

    Add Google analytics

---
+++ datamung-web/src/main/webapp/js/google-analytics-boot.js
@@ -0,0 +1,14 @@
+var _gaq = _gaq || [];
+_gaq.push([ '_setAccount', 'UA-37335911-1' ]);
+_gaq.push([ '_setDomainName', 'cyclopsgroup.org' ]);
+_gaq.push([ '_trackPageview' ]);
+
+(function() {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://')
+			+ 'stats.g.doubleclick.net/dc.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
