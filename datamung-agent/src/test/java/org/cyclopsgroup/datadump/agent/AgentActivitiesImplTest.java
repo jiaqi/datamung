38fe955b1d43217627250aa1dab344aef84e0db1
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2015-04-28 12:52:42 -0400

    Ignore platform specific tests on windows

--- datamung-agent/src/test/java/org/cyclopsgroup/datadump/agent/AgentActivitiesImplTest.java
+++ datamung-agent/src/test/java/org/cyclopsgroup/datadump/agent/AgentActivitiesImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
+import org.apache.commons.lang.SystemUtils;
 import org.cyclopsgroup.datamung.agent.AgentActivitiesImpl;
 import org.cyclopsgroup.datamung.swf.types.CommandLineJob;
 import org.cyclopsgroup.datamung.swf.types.JobResult;
@@ -11,19 +12,31 @@ import org.junit.Test;
 
 public class AgentActivitiesImplTest
 {
+    private static boolean isWindows()
+    {
+        return SystemUtils.OS_NAME.toLowerCase().startsWith( "windows" );
+    }
+
     @Test
     public void testRunJobWithPwd()
     {
+        if ( isWindows() )
+        {
+            return;
+        }
         CommandLineJob job = new CommandLineJob();
         job.setCommand( "pwd" );
         JobResult result = new AgentActivitiesImpl().runJob( job );
-        assertEquals( new File( "" ).getAbsoluteFile(),
-                      new File( result.getStandardOutput() ).getAbsoluteFile() );
+        assertEquals( new File( "" ).getAbsoluteFile(), new File( result.getStandardOutput() ).getAbsoluteFile() );
     }
 
     @Test
     public void testRunJobWithEcho()
     {
+        if ( isWindows() )
+        {
+            return;
+        }
         CommandLineJob job = new CommandLineJob();
         job.setCommand( "echo 1 2 3" );
         JobResult result = new AgentActivitiesImpl().runJob( job );
